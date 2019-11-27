9f6e6a4cb9b248335c249f81c1dde3c6d74e0788
Ku-Jui-An <50874793+Ku-Jui-An@users.noreply.github.com>
2019-11-28 03:59:45 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -15,25 +15,6 @@ var fs = require('fs');
 var routes = require('./routes/index');
 var usersRouter = require('./routes/users');
 var login = require('./routes/login');
-// var mysql = require('mysql');
-
-// var connection = mysql.createConnection({
-//   host     : '35.213.23.117',
-//   port     : '3306',
-//   user     : 'Andrew',
-//   password : 'Andrew93220@',
-//   database : 'Notebook'
-// });
-
-
-//連接資料庫
-// connection.connect(function(err) {
-//   if (err) {
-//     console.log('connecting error '+ err);
-//     return;
-//   }
-//   console.log('connecting success');
-// });
 
 var mysql = require('mysql');
 var mysql_config={
@@ -69,7 +50,7 @@ handleDisconnection()
 
 app.use(logger('dev'));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Correction-Note')));
 app.use(bodyParser.json({limit:"2100000kb"}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/',login);
@@ -102,26 +83,40 @@ app.use(function(req, res, next) {
 app.use('/routes',routes);
 app.use('/users', usersRouter);
 
-
-
-var storage = multer1.diskStorage({
-  //儲存的位址，會儲存在uploads的目錄裡
-  destination: function (req, file, cb) {
-    cb(null, 'uploads')
-  },
-  //修改檔名，最後的.jpg可以改成其他格式
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now()+'.jpg')
-  }
+const pool = mysql.createPool({
+  host: '35.206.219.27',
+  user: 'Andrew',
+  password: 'Andrew93220@',
+  database: 'Notebook'
 });
 
-var upload = multer1({ storage: storage });
+let query = function (sql) {
+  return new Promise((resolve, reject) => {
+    pool.getConnection(function (err, connection) {
+      if (err) {
+        reject(err)
+      } else {
+        connection.query(sql, (err, rows) => {
+
+          if (err) {
+            reject(err)
+          } else {
+            resolve(rows)
+          }
+          connection.release()
+        })
+      }
+    })
+  })
+};
+
+module.exports = query;
 
 //單圖上傳
 //upload.single('file')的file盡量跟<input>的name一致
-app.post('/uploads', function(req, res,next){
+app.post('/uploads', function (req, res, next) {
   let data_url = req.body.baseimg;
-//var buff = new Buffer.from(data_url.replace(/^data:image\/(png|gif|jpeg);base64,/,''), 'base64');
+  //var buff = new Buffer.from(data_url.replace(/^data:image\/(png|gif|jpeg);base64,/,''), 'base64');
 
   let matches = data_url.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
       response = {};
@@ -131,23 +126,44 @@ app.post('/uploads', function(req, res,next){
   response.type = matches[1];
   response.data = new Buffer.from(matches[2], 'base64');
   console.log(dateString);
-  fs.writeFile(path.join(__dirname,'/uploads/',dateString+"."+response.type.split("/")[1]), response.data, function(error){
-    if(error){
+  let picname = dateString + "." + response.type.split("/")[1];
+  fs.writeFile(path.join(__dirname, '/Correction-Note/images', picname), response.data, function (error) {
+    if (error) {
       throw error;
-    }else{
+    } else {
       console.log('File created from base64 string!');
       return true;
     }
   });
-  if(req.body.toText){
-    test(__dirname+'/uploads/'+dateString+"."+response.type.split("/")[1],res);
+  if (req.body.toText) {
+    test(__dirname + '/Correction-Note/images' + dateString + "." + response.type.split("/")[1], res);
+  }
+  else{
+    var picmark = req.body.mark;
+    var noteid = req.body.note_id;
+    var picpath="http://35.206.219.27:3000/images/"+picname;
+    if(picmark="answer"){
+      var sqldata = {
+        answer_pic:picpath
+      }
+    }else if(picmark="question"){
+      var sqldata = {
+        question_pic:picpath
+      }
+    }
+    console.log(sqldata);
+    var sql = "UPDATE note SET ? WHERE id = ?";
+    connection.query(sql,[sqldata,noteid],function(err,rows){
+      if(err)
+        console.log("圖片路徑添加失敗"+err);
+      else
+        console.log("圖片路徑添加成功");
+    });
   }
 });
 
 async function test(urls,res) {
-
   const vision = require('@google-cloud/vision');
-
 // Creates a client
   const client = new vision.ImageAnnotatorClient();
   const [result] = await client.textDetection(urls);
@@ -164,22 +180,57 @@ async function test(urls,res) {
   res.json(translateText);
 }
 
-app.get("/readimage",function(req,res,next){
-  client
-      .textDetection('2019-10-15-13-14-33.jpeg')
-      // Creates a client
-      .then(results =>{
-        var final = [];
-        const labels = results[0].textAnnotations;
-        console.log('Labels:');
-        // labels.forEach(function(answer){
-        //         final=answer;
-        // });
-        res.json(labels);
-      })
-      .catch(err =>{
-        console.error('ERROR:',err);
-      })
+app.post('/getNote', async function (req, res, next) {
+  var range = 10;
+  var sql2 = 'Select * From note where user_id=' + req.body.id + ' ORDER BY id DESC Limit ' + req.body.floor + ',' + range;
+  var sql3 ='SELECT COUNT(*) as total FROM note where user_id = '+ req.body.id;
+  let sql;
+  let notId;
+  let returnMainData = [];
+  let max;
+  let maxsql = await query(sql3);
+  maxsql.forEach(async function(index,value) {
+    max = index.total;
+  });
+  returnMainData["main"] = {};
+  let rows = await query(sql2);
+  rows.forEach(async function (index, value) {
+    let name = "data" + value;
+    returnMainData["main"][name];
+    returnMainData["main"][name] = {
+      "id": index.id,
+      "title": index.title,
+      "content": index.content
+    };
+    notId = index.id;
+  });
+  console.log(notId);
+  let name = "";
+  sql2="select * from mark where note_id="+ notId;
+  rows = await query(sql2);
+
+  rows.forEach(async function (index, value) {
+    sql = rows[value].label_id;
+
+    name = "data" + value;
+    returnMainData["main"][name]["tag"] = [];
+  });
+  console.log("tagid="+sql);
+  sql2 ="select name from label where id="+sql;
+  rows = await query(sql2);
+
+  rows.forEach(async function (index, value) {
+    console.log(rows.length)
+    console.log(value)
+    returnMainData["main"][name]["tag"].push(rows[value].name);
+  });
+  res.json({
+    max:max,
+    note:returnMainData["main"]
+  });
+
+  const data = await returnMainData;
+
 });
 
 
